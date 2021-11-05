329a5f46cb121821c6f6fc8827a6c5925b979c3c
karenjimenez27 <karenjimenez980527@gmail.com>
2021-11-04 20:49:16 -0500

    Crecacion de servicios y controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,6 @@ export * from './ping.controller';
 export * from './pedidos-cliente.controller';
 export * from './cliente-pedidos.controller';
 export * from './pedidos-producto.controller';
+export * from './cliente.controller';
+export * from './producto.controller';
+export * from './pedidos.controller';
